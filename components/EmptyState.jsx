023510fcc52f8496c3abd7c3039cc99c4c3a1734
Aryan Chouksey <ayushchauksey08@gmail.com>
2024-07-17 19:06:49 +0530

    LogIn Home and Profile Page Done

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -1,31 +1,23 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { router } from 'expo-router'
-import { images } from '../constants'
-import CustomButton from './CustomButton'
+import { Image, Text, View } from "react-native";
+import React from "react";
+import { images } from "../constants";
 
-const EmptyState = ({title,subtitle,buttontitle,buttonlinkedpage}) => {
+const EmptyState = ({ title, subtitle }) => {
   return (
     <View className="justify-center items-center px-4">
-        <Image
-            source={images.empty} 
-            className="w-[270px] h-[215px]"
-            resizeMode='contain'
-        />
-        <Text className="font-psemibold text-center text-xl text-white mt-2">
-            {title}
-        </Text>
-        <Text className="font-pmedium text-sm text-gray-100">
-            {subtitle}
-        </Text>
-       
-       <CustomButton
-        title={buttontitle}
-        handlePress={() => router.push(buttonlinkedpage)}
-        containerStyles="w-full my-5"
-       />
+      <Image
+        source={images.empty}
+        className="w-[270px] h-[250px]"
+        resizeMode="contain"
+      />
+      <Text className="text-xl text-center font-psemibold text-white">
+        {title}
+      </Text>
+      <Text className="font-pmedium mt-2 text-sm text-gray-100">
+        {subtitle}
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default EmptyState
+export default EmptyState;
