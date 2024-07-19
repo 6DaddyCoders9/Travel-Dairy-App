88206f06e7f66071adf119df3e1b6f8b6975a3ad
Aryan Chouksey <ayushchauksey08@gmail.com>
2024-07-19 19:50:44 +0530

    Database Added (#8)

---
+++ app/(tabs)/travel-tips.jsx
@@ -0,0 +1,13 @@
+// screens/TravelTipsPage.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TravelTips = () => {
+  return (
+    <View>
+      <Text>Travel Tips</Text>
+    </View>
+  );
+};
+
+export default TravelTips;
